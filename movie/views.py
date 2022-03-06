3008e3a88b2a5d9ee3b1e1a537302758e4504876
jiho5993 <jiho5993@naver.com>
2022-03-06 14:22:40 +0900

    test:

--- movie/views.py
+++ movie/views.py
@@ -39,62 +39,113 @@ def movie_info(request):
     movie = MovieAPI()
 
     if request.method == 'POST':
-        result = movie.movie_info_naver(query)
+        # result = movie.movie_info_naver(query)
 
-        if result == False:
-            return JsonResponse(
-                {
-                    "success": False
-                }
-            )
+        # if result == False:
+        #     return JsonResponse(
+        #         {
+        #             "success": False
+        #         }
+        #     )
 
-        result = sorted(result, key=(lambda x: x['userRating']), reverse=True)
-        movie_card = []
+        # result = sorted(result, key=(lambda x: x['userRating']), reverse=True)
+        # movie_card = []
 
-        for mov in result:
-            # if mov["isSuccess"] is False:
-            #     continue
+        # for mov in result:
+        #     # if mov["isSuccess"] is False:
+        #     #     continue
 
-            actors = mov['actor'].split('|')[:2]
-            actors = ", ".join(actors) + " 등"
+        #     actors = mov['actor'].split('|')[:2]
+        #     actors = ", ".join(actors) + " 등"
 
-            directors = mov['director'].split('|')[:2]
-            directors = ", ".join(directors)
+        #     directors = mov['director'].split('|')[:2]
+        #     directors = ", ".join(directors)
 
-            description = "⭐ " + str(mov['userRating']) + "\n" \
-                + "· 개요 " + ("장르 없음" if mov['genre'] == None else mov['genre']) + " | " + "\n" \
-                + ("국가 없음" if mov['nation'] == None else mov['nation']) + " | " + "\n" \
-                + ("러닝타임 없음" if mov["playtime"] == None else mov["playtime"]) + "\n" \
-                + "· 감독 " + directors + "\n" \
-                + "· 출연 " + actors + "\n" \
-                + "· 등급 " + ("연령 등급 없음" if mov['age'] == None else mov['age'])
+        #     description = "⭐ " + str(mov['userRating']) + "\n" \
+        #         + ("장르 없음" if mov['genre'] == None else mov['genre'])
+        #         # + ("국가 없음" if mov['nation'] == None else mov['nation']) + " | " \
+        #         # + ("러닝타임 없음" if mov["playtime"] == None else mov["playtime"]) + "\n" \
+        #         # + "· 감독 " + directors + "\n" \
+        #         # + "· 출연 " + actors + "\n" \
+        #         # + "· 등급 " + ("연령 등급 없음" if mov['age'] == None else mov['age'])
 
-            movie_card.append(
-                {
-                    "title": mov['title'],
-                    "description": description,
-                    "thumbnail": {
-                        "imageUrl": mov['image']
-                    },
-                    "buttons": [
-                        {
-                            "action": "webLink",
-                            "label": "상세 정보 주소",
-                            "webLinkUrl": mov['link']
-                        }
-                    ]
-                }
-            )
+        #     movie_card.append(
+        #         {
+        #             "title": mov['title'],
+        #             "description": description,
+        #             "thumbnail": {
+        #                 "imageUrl": mov['image']
+        #             },
+        #             "buttons": [
+        #                 {
+        #                     "action": "webLink",
+        #                     "label": "상세 정보 주소",
+        #                     "webLinkUrl": mov['link']
+        #                 },
+        #                 {
+        #                     "action": ""
+        #                 }
+        #             ]
+        #         }
+        #     )
 
         return JsonResponse(
-            {
+           {
                 "version": "2.0",
                 "template": {
                     "outputs": [
                         {
-                            "carousel": {
-                                "type": "basicCard",
-                                "items": movie_card
+                            "itemCard": {
+                                "imageTitle": {
+                                    "title": "DOFQTK",
+                                    "description": "Boarding Number"
+                                },
+                                "title": "",
+                                "description": "",
+                                "thumbnail": {
+                                    "imageUrl": "https://w.namu.la/s/45507892b4f48b2b3d4a6386f6dae20c28376a8ef5dfb68c7cc95249ec358e3e68df77594766021173b2e6acf374b79ce02e9eeef61fcdf316659e30289e123fbddf6e5ec3492eddbc582ee5a59a2ff5d6ee84f57ad19277d179b613614364ad",
+                                    "width": 800,
+                                    "height": 800
+                                },
+                                "profile": {
+                                    "title": "AA Airline",
+                                    "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/aaairline.jpg"
+                                },
+                                "itemList": [
+                                    {
+                                        "title": "Flight",
+                                        "description": "KE0605"
+                                    },
+                                    {
+                                        "title": "Boards",
+                                        "description": "8:50 AM"
+                                    },
+                                    {
+                                        "title": "Departs",
+                                        "description": "9:50 AM"
+                                    },
+                                    {
+                                        "title": "Terminal",
+                                        "description": "1"
+                                    },
+                                    {
+                                        "title": "Gate",
+                                        "description": "C24"
+                                    }
+                                ],
+                                "itemListAlignment" : "right",
+                                "itemListSummary": {
+                                    "title": "Total",
+                                    "description": "$4,032.54"
+                                },
+                                "buttons": [
+                                    {
+                                        "label": "View Boarding Pass",
+                                        "action": "webLink",
+                                        "webLinkUrl": "https://namu.wiki/w/%EB%82%98%EC%97%B0(TWICE)"
+                                    }
+                                ],
+                                "buttonLayout" : "vertical"
                             }
                         }
                     ]
